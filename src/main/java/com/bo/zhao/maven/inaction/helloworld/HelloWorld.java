4b977351a4030f47f9c6d927ea0919c2c7aa2ad4
xszhaobo <azhaobo1991@163.com>
2017-09-04 23:46:54 +0800

    更新忽略文件的数量

---
+++ src/main/java/com/bo/zhao/maven/inaction/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bo.zhao.maven.inaction.helloworld;
+
+/**
+ * ÎÄ¼şÃèÊö£º
+ *
+ * @author Bo.Zhao
+ * @version 3.0
+ * @since 17/9/4
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
