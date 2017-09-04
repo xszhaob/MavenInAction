defde560dde10cdfd3ab36d13ffb6d0c747daa6c
xszhaobo <azhaobo1991@163.com>
2017-09-04 23:49:38 +0800

    Maven实战入门

---
+++ src/test/java/com/bo/zhao/maven/inaction/test/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package com.bo.zhao.maven.inaction.test;
+
+import com.bo.zhao.maven.inaction.helloworld.HelloWorld;
+import org.junit.Test;
+
+/**
+ * 文件描述：
+ *
+ * @author Bo.Zhao
+ * @version 3.0
+ * @since 17/9/4
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void test() {
+        HelloWorld.main(null);
+    }
+}
